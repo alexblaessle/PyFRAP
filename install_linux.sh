0a4fa54fefefe75e6649ce85abe4de6a1c16c6aa
alexblaessle <alexander.blaessle@gmail.com>
2019-11-05 22:44:27 +0100

    Changed linux installation script

--- install_linux.sh
+++ install_linux.sh
@@ -4,19 +4,19 @@
 sudo apt-get install -y git
 sudo apt-get install default-jdk
 sudo apt-get install default-jre
-sudo apt-get install -y python-numpy
-sudo apt-get install -y python-scipy
-sudo apt-get install -y python-matplotlib
-sudo apt-get install -y python-qt5
-sudo apt-get install -y python-skimage
-sudo apt-get install -y python-vtk
+sudo apt-get install -y python3-numpy
+sudo apt-get install -y python3-scipy
+sudo apt-get install -y python3-matplotlib
+sudo apt-get install -y python3-qt5
+sudo apt-get install -y python3-skimage
+sudo apt-get install -y python3-vtk
 
 # Install everything that is in pip
-sudo pip install fipy
-sudo pip install solidpython
-sudo pip install numpy-stl
-sudo pip install wget
-sudo pip install python-bioformats
+sudo pip3 install fipy
+sudo pip3 install solidpython
+sudo pip3 install numpy-stl
+sudo pip3 install wget
+sudo pip3 install python-bioformats
 
 # Install openscad
 sudo apt-get install openscad
